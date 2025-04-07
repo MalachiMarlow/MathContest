a6c35901519ff7e14b62af4a4fc818df3eeda241
Tusk <Tuskman045@gmail.com>
2025-04-06 20:55:13 -0600

    Add project files.

---
+++ MathContest/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MathContest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University")>
+<Assembly: AssemblyProduct("MathContest")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f9800d4d-4024-4478-b36a-9ff0f93b5263")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
